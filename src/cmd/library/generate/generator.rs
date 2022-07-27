1da81456b17ffcdcdefa3f885df1c0c1fa10d926
chalmagr <4968250+chalmagr@users.noreply.github.com>
2022-07-27 17:27:22 -0500

    using counter.get

--- src/cmd/library/generate/generator.rs
+++ src/cmd/library/generate/generator.rs
@@ -80,7 +80,7 @@ impl Generator {
         log::info!("Start the Create Resources phase.");
         let mut counter = Counter::start(self.tasks.len());
         for task in &self.tasks {
-            log::info!("create_resources on task #{}", counter);
+            log::info!("create_resources on task #{}", counter.get());
             task.create_resources()?;
             counter.increase();
         }
